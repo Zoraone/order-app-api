a180c6a647adcdb7761dcae7732765b03a4ed28e
Peng Zhong <Zoraone@users.noreply.github.com>
2019-11-02 13:16:51 +1300

    Add cors to getcartsinorder

--- order/controller.go
+++ order/controller.go
@@ -114,6 +114,7 @@ func (c *Controller) GetCartsInOrder(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(carts)
 }
